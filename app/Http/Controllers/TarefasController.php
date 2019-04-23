dabd142dbf68b272065bda85a5746bf92b3f325c
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2019-04-23 18:20:15 -0300

    Criação dos métodos da API
    Criação do criar, editar, exibir e excluir

--- app/Http/Controllers/TarefasController.php
+++ app/Http/Controllers/TarefasController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Tarefa;
+use Validator;
+use Illuminate\Validation\Rule;
+
 
 class TarefasController extends Controller
 {
@@ -21,7 +24,30 @@ class TarefasController extends Controller
 
     public function store(Request $request)
     {
-        //
+
+       //Simples validação dos campos da requisição
+       $validator = Validator::make($request->all(), [
+            'titulo' => 'required',
+            'descricao' => 'required|max:255',
+            'status' => [
+                 'required',
+                 Rule::in([1,2,3,4])
+            ]
+        ]);
+
+        if ($validator->fails()) {
+
+            return response()->json($validator->errors(), 400, array());
+        }
+
+        $tarefa = new Tarefa();
+        $tarefa->titulo = $request->titulo;
+        $tarefa->descricao = $request->descricao;
+        $tarefa->status = $request->status;
+
+        $tarefa->save();
+
+        return response()->json($tarefa, 201);
     }
 
     /**
@@ -32,7 +58,14 @@ class TarefasController extends Controller
      */
     public function show($id)
     {
-        //
+        $tarefa = Tarefa::find($id);
+
+        if(empty($tarefa)){
+            return response()->json('Tarefa não encontrada', 404);
+        }
+
+        return response()->json($tarefa, 200);
+
     }
 
 
@@ -45,7 +78,33 @@ class TarefasController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+       //Simples validação dos campos da requisição
+       $validator = Validator::make($request->all(), [
+            'titulo' => 'required',
+            'descricao' => 'required|max:255',
+            'status' => [
+                 'required',
+                 Rule::in([1,2,3,4])
+            ]
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400, array());
+        }
+
+         $tarefa = Tarefa::find($id);
+
+        if(empty($tarefa)){
+            return response()->json('Tarefa não encontrada', 404);
+        }
+
+        $tarefa->titulo = $request->titulo;
+        $tarefa->descricao = $request->descricao;
+        $tarefa->status = $request->status;
+
+        $tarefa->save();
+
+        return response()->json($tarefa, 201);
     }
 
     /**
@@ -56,6 +115,14 @@ class TarefasController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $tarefa = Tarefa::find($id);
+
+        if(empty($tarefa)){
+            return response()->json('Tarefa não encontrada', 404);
+        }
+
+        $tarefa->delete();
+        return response()->json('Tarefa deletada com sucesso', 200);
+
     }
 }
