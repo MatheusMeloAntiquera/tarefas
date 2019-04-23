2d55c8cea77d4d9d2a59b1c9353554fa6b29b3f8
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2019-04-23 17:08:09 -0300

    Ajustes e criação da listagem
    Criação dos controller, model e rota e também a listagem de tarefas

---
+++ app/Http/Controllers/TarefasController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Tarefa;
+
+class TarefasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tarefas = Tarefa::all();
+        return response()->json($tarefas, 200);
+
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
